7dccabc569699f82319210bf9852d4a22d5aa453
Senott <senott@yahoo.com>
2020-10-22 23:40:23 -0300

    Adjustment to return coffee shops 1 km away from user location.

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::StoresController < ApplicationController
     @stores = Store.within(
       params[:longitude].to_f,
       params[:latitude].to_f,
-      params[:distance_in_km].to_i
+      params[:distance_in_km] ? params[:distance_in_km].to_i : 1
     ).sort_by { |store| store.ratings_average }.reverse
   end
 
