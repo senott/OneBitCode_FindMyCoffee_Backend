b02e65d772b9f240e58f6c71c97500699f9eef07
Senott <senott@yahoo.com>
2020-10-24 15:11:48 -0300

    Details service adjustment, unnecessary variables declared removed.

--- app/services/get_google_coffee_details_service.rb
+++ app/services/get_google_coffee_details_service.rb
@@ -9,7 +9,6 @@ class GetGoogleCoffeeDetailsService
   def execute
     begin
       key = Rails.application.credentials.google_secret_key
-      location = "#{@latitude},#{@longitude}"
       base_url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=#{@google_place_id}&key=#{key}"
 
       response = RestClient.get(base_url)
